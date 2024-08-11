0d94292686f8e9016a1bac1c1f807191051f224b
keisuke sato <corydoras121@gmail.com>
2024-08-11 11:52:30 +0900

    first

---
+++ src/js/main.js
@@ -0,0 +1,145 @@
+// import '../css/style.scss';
+// import { radian, random } from './utils';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { VivianiCurve } from "three/examples/jsm/curves/CurveExtras.js"
+
+
+class Main {
+  constructor() {
+    this.viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+
+    this.canvas = document.querySelector("#canvas");
+
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+
+    this.scene = new THREE.Scene();
+    this.camera = null;
+    this.mesh = null;
+
+    this.controls = null;
+
+
+    this.percentage = 0;
+
+    this._init();
+    this._update();
+    this._addEvent();
+  }
+
+  _setCamera() {
+    // this.camera = new THREE.PerspectiveCamera(45, this.viewport.width / this.viewport.height, 1, 100);
+    // this.camera.position.set(0, 0, 5);
+    // this.scene.add(this.camera);
+
+    //ウインドウとWebGL座標を一致させる
+    const fov = 45;
+    const fovRadian = (fov / 2) * (Math.PI / 180); //視野角をラジアンに変換
+    const distance = (this.viewport.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
+    this.camera = new THREE.PerspectiveCamera(fov, this.viewport.width / this.viewport.height, 1, distance * 2);
+    this.camera.position.z = distance;
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+    this.scene.add(this.camera);
+  }
+
+  _setControlls() {
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.enableDamping = true;
+  }
+
+  _setLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(1, 1, 1);
+    this.scene.add(light);
+  }
+
+  _setCurve() {
+    this.curveLine = new THREE.CatmullRomCurve3([
+      new THREE.Vector3( 0, - 40, - 40 ),
+      new THREE.Vector3( 0, 40, - 40 ),
+      new THREE.Vector3( 0, 100, - 40 ),
+      new THREE.Vector3( 0, 40, 40 ),
+      new THREE.Vector3( 0, - 40, 40 )
+    ]);
+    this.curveLine.curveType = 'catmullrom';
+    this.curveLine.curveType = 'chordal';
+    this.curveLine.closed = true;
+    this.curveLine.tension = 20.0;
+
+
+
+    this.curveLineVivian = new VivianiCurve( 70 );
+  }
+
+  _addMesh() {
+    // const geometry = new THREE.BoxGeometry(50, 50, 50);
+    const geometry = new THREE.TubeGeometry(this.curveLineVivian, 100, 4, 10, false);
+    const material = new THREE.MeshStandardMaterial({color: 0x444444, wireframe: true });
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(this.mesh);
+  }
+
+  _init() {
+    this._setCamera();
+    this._setControlls();
+    this._setLight();
+    this._setCurve();
+    this._addMesh();
+  }
+
+  _update() {
+    // this.mesh.rotation.y += 0.01;
+    // this.mesh.rotation.x += 0.01;
+
+    this.percentage += 0.001;
+
+    let p1 = this.curveLineVivian.getPoint(this.percentage%1);
+    // let p2 = this.curveLineVivian.getPointAt((this.percentage + 0.01)%1);
+    let p2 = this.curveLineVivian.getPoint((this.percentage + 0.01)%1);
+
+    this.camera.position.set(p1.x, p1.y, p1.z);
+    this.camera.lookAt(p2);
+
+    // カメラの位置と向きをスムーズに変更
+    // this.camera.position.lerp(p1, 0.1);
+    // const direction = new THREE.Vector3().subVectors(p2, p1).normalize();
+    // this.camera.lookAt(p1.clone().add(direction));
+
+    
+
+
+    //レンダリング
+    this.renderer.render(this.scene, this.camera);
+    this.controls.update();
+    requestAnimationFrame(this._update.bind(this));
+  }
+
+  _onResize() {
+    this.viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+    // レンダラーのサイズを修正
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+    // カメラのアスペクト比を修正
+    this.camera.aspect = this.viewport.width / this.viewport.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  _addEvent() {
+    window.addEventListener("resize", this._onResize.bind(this));
+  }
+}
+
+new Main();
+
+
+
